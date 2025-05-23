48b10fc9a7012c461938c59f2de9f008cc3ac5b9
Eric Shi <origami1685@gmail.com>
2025-05-23 14:08:07 -0700

    initialize

---
+++ motor_stand_master/src/motor_stand_master.cpp
@@ -0,0 +1,284 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Keypad.h>
+#include <LiquidCrystal_I2C.h>
+#include <Servo.h>
+
+////////////////////////////////////////////////////////////////////////////////////////
+//LCD I2C address: 0x27
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 20 chars and 4 line display
+
+////////////////////////////////////////////////////////////////////////////////////////
+//KEYPAD AND I/O DEFINITIONS
+
+//KEYBOARD INPUT (4x4 Membrane keypad):
+const byte ROWS = 4; // rows
+const byte COLS = 4; // columns
+
+//change these values to change keymapping for starting data collection, stopping data collection, entering test number, and sending test number
+const char START_INPUT = 'A'; 
+const char CONFIRM_TARE = 'B';
+const char ENTER_INPUT = '#';
+const char SEND_INPUT = '*';
+ 
+// Define the keymap
+char keys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+byte rowPins[ROWS] = {10, 11, 12, 13};
+byte colPins[COLS] = {6, 7, 8, 9};
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+String file_num;
+bool input_done;
+bool file_created;
+
+////////////////////////////////////////////////////////////////////////////////////////
+//THROTTLE LOGIC DEFINITIONS
+/*
+For HARGRAVE MICRODRIVE LP. Accepted PWM frequencies are from 50Hz to 499 Hz
+*/
+Servo esc; 
+
+const int ESC_PIN = 3;
+const int MIN_THROTTLE = 1000;
+const int MAX_THROTTLE = 2000;
+const int ARMING_DELAY = 2000;
+const int INCREMENT_TIME = 4000;
+
+String increment_input;
+int throttleIncrement;
+int pwm_increment;
+int cycle_length;
+
+bool increment_done;
+bool start_motor; //tells the program to start throttling up the motor
+bool done_throttling;
+bool throttling_up;
+bool interrupted;
+long prev_interval_timestamp;
+
+const int THROTTLE_UP_DELAY = 10;
+
+const int INTERRUPT_PIN = 2;
+const int ARM_PIN = 1;
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+bool tared = false; //only reset taring status after the entire system has been unpowered/restarted
+
+////////////////////////////////////////////////////////////////////////////////////////
+//HELPER FUNCTIONS
+
+void lcd_home(){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Test #:");
+  lcd.setCursor(0, 1);
+  lcd.print("Test Status:OFF ");
+  lcd.setCursor(0, 2);
+  lcd.print("File Status:NO FILE");
+  lcd.setCursor(0, 3);
+  lcd.print("Increment:");
+  lcd.setCursor(7, 0);
+}
+
+void tare_ui(){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("V:"); 
+  lcd.setCursor(0, 1);
+  lcd.print("I:");
+  lcd.setCursor(0, 2);
+  lcd.print("T:");
+  lcd.setCursor(0, 3);
+  lcd.print("R:");
+
+  Wire.beginTransmission(9);
+  Wire.write('t');
+  Wire.endTransmission();
+}
+
+void send_file(){
+  lcd.setCursor(12, 2);
+  lcd.print("EXISTS ");
+  file_created = true;
+  Wire.beginTransmission(9);
+  Wire.write(file_num.c_str());
+  Wire.endTransmission();
+}
+
+void start_testing(){
+  Serial.println("Starting: Test Num: " + file_num + " | Increment: " + String(throttleIncrement));
+  lcd.setCursor(12, 1);
+  lcd.print("ON   ");
+  start_motor = true;
+  Wire.beginTransmission(9);
+  Wire.write('b');
+  Wire.endTransmission();
+}
+
+void end_testing(){
+  Wire.beginTransmission(9);
+  Wire.write('e');
+  Wire.endTransmission();
+  setup();
+}
+
+void throttle_up(){
+  if(millis() >= prev_interval_timestamp + INCREMENT_TIME && cycle_length >= MAX_THROTTLE){
+    Serial.println("DONE THROTTLING");
+    done_throttling = true;
+  }
+  else if(millis() >= prev_interval_timestamp + INCREMENT_TIME){
+    Serial.println(cycle_length);
+    for(int i = cycle_length; i <= min(cycle_length + pwm_increment, MAX_THROTTLE); i++){
+      esc.writeMicroseconds(i);
+      delay(THROTTLE_UP_DELAY);
+    }
+    cycle_length = min(cycle_length + pwm_increment, MAX_THROTTLE);
+    prev_interval_timestamp = millis();
+  }
+}
+
+void throttle_down(){
+  for(int i = cycle_length; i >= MIN_THROTTLE; i--){
+    esc.writeMicroseconds(i);
+    delay(THROTTLE_UP_DELAY);
+  }
+  end_testing();
+}
+
+void interrupt(){
+  interrupted = true;
+}
+
+void check_interrupt(){
+  if(interrupted){
+    Serial.println("Manual Stop");
+    interrupted = false;
+    throttle_down();
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//MAIN DRIVER CODE
+
+void setup() {
+  file_num = "";
+  input_done = false;
+  file_created = false;
+
+  increment_input = "";
+  done_throttling = false;
+  throttling_up = false;
+  start_motor = false;
+  increment_done = false;
+  cycle_length = MIN_THROTTLE;
+  interrupted = false;
+  
+  //Initialize Serial 
+  Serial.begin(9600);
+  Serial.println("Setting up");
+  delay(3000); //account for delay on the slave for load cell initialization/taring
+  //DO NOT LOAD THE TORQUE SENSOR UNTIL LCD SCREEN TURNS ON
+  //LOADING THE LOAD CELL WHILE THE PROGRAM INITIALIZES IT WILL RESULT IN INACCURATE READINGS
+
+  pinMode(INTERRUPT_PIN, INPUT_PULLUP); //set default switch position to HIGH
+  pinMode(ARM_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), interrupt, FALLING); //when switch is pressed down
+
+  // Set up the LCD display
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();
+  if(tared){
+    lcd_home();
+  }
+  else{
+    tare_ui();
+  }
+
+  //Initialize I2C protocol (master)
+  Wire.begin();
+
+  //Initialize servo PWM and arm the ESC
+  esc.attach(ESC_PIN); //set esc to pin
+  esc.writeMicroseconds(MIN_THROTTLE); //minimum throttle; arm the esc
+  delay(ARMING_DELAY);
+}
+
+void loop() {
+  check_interrupt();
+  char key = keypad.getKey();
+
+  if(!tared){
+    //TODO: get torque/RPM data from slave with Wire.request
+    if(key && key == CONFIRM_TARE){ //the button to zero the values
+      Wire.beginTransmission(9);
+      Wire.write('z');
+      Wire.endTransmission();
+      tared = true;
+      lcd_home(); //reset display to its normal "home" state
+    }
+  }
+  else{ //can only do everything else once sensors have been tared
+    if(start_motor){
+      throttle_up();
+    }
+
+    if(done_throttling){
+      throttle_down();
+    }
+
+    //if a keystroke has been entered from the keypad
+    if(key){
+      //If user is still entering test number (AKA test number NOT confirmed yet)
+      if(!input_done){
+        if(key >= '0' && key <= '9' && file_num.length() < 3){ //if a digit is being entered and the test number is less or equal to 3 digits long, add digit to test number
+          file_num += key;
+          lcd.print(key);
+        }
+        else if(key == ENTER_INPUT){ //If the user confirms the test number, no further digits can be typed in; The user is only able to send files, start testing, and stop testing from now on
+          lcd.print(ENTER_INPUT);
+          input_done = true; 
+        }
+      }
+      //if test number has been finalized (entered in) by the user
+      //this ensures starting, stopping, and creating a logging file can only be done if the user confirmed the test number
+      else {
+        if(!file_created){ //if a file hasn't been created/sent to the slave yet...
+          if(key == SEND_INPUT){ //send test number to the data collection arduino
+              send_file();
+          }
+        }
+        //This ensures that data recording can only commence once the file has been created
+        else {
+          if(!increment_done){
+            lcd.setCursor(10 + increment_input.length(), 3);
+            if(key >= '0' && key <= '9' && increment_input.length() < 2){ //set the throttle increment. Keep it below 100%
+              if(!(increment_input.length() == 0 && key == '0')){ //making sure no goofy things with starting with zero
+                lcd.print(key);
+                increment_input += key;
+              }
+            }
+            else if(key == ENTER_INPUT){
+              lcd.print(ENTER_INPUT);
+              throttleIncrement = increment_input.toInt(); //set the throttleIncrement to what the user inputted and display it on the info screen
+              pwm_increment = map(throttleIncrement, 0, 100, 0, MAX_THROTTLE - MIN_THROTTLE); //1% -> 99% written in terms of PWM cycle length, assuming a linear mapping
+              increment_done = true;
+            }
+          }
+          else if(key == START_INPUT){ //tell data collection to start by sending 'b' for begin
+            start_testing();
+          }
+        }      
+      }
+    }
+  }
+}
---
+++ motor_stand_slave/src/motor_stand_slave.cpp
@@ -0,0 +1,230 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <SD.h>
+#include <HX711_ADC.h>   
+                          
+///////////////////////////////////////////////////////////////////////////////////////
+// DEFINITIONS
+///////////////////////////////////////////////////////////////////////////////////////
+
+const int TORQUE_DOUT_PIN = 5;    // mcu > hx711 data out pin
+const int TORQUE_SCK_PIN = 6;   // mcu > hx711 serial clock pin
+
+const int CURRENT_PIN = A2;
+const int VOLTAGE_PIN = A3;
+const int CURRENT_SENSITIVITY = 0.020;
+const int CURRENT_SENSITIVITY_NEW =  0.022;
+const int ZERO_CURRENT_VOLTAGE = 0.22;
+
+const int KNOWN_TORQUE = 1; //change this to update zeroing settings
+//TODO: Use i2C to send knwon torque parameter form master to the slave
+// by adding a 'q' character before the other numbers in the signal to send
+// this way, the slave knows that it is recieving a knwon torque parameter signal 
+// by seeing that the signal starts iwth a q character
+
+// CALIBRATION DEFINITIONS
+
+#define MANUAL_TORQUE_CALIBRATION_VALUE 1.0     // manual torque calibration value (float)
+
+#define SERIAL_PRINT_INTERVAL 300.0      // interval between each printed value
+
+///////////////////////////////////////////////////////////////////////////////////////
+// CONSTRUCTORS
+///////////////////////////////////////////////////////////////////////////////////////
+
+HX711_ADC TorqueSensor(TORQUE_DOUT_PIN, TORQUE_SCK_PIN);
+
+///////////////////////////////////////////////////////////////////////////////////////
+// VARIABLE INSTANTIATIONS
+///////////////////////////////////////////////////////////////////////////////////////
+
+// GENERAL VARIABLES
+unsigned long last_serial_timestamp = 0;
+
+// CALIBRATION VARIABLES
+boolean bool_tare_next;
+
+float thrust_calibration_value;
+float torque_calibration_value;
+
+///////////////////////////////////////////////////////////////////////////////////////
+//INPUT AND WRITING DEFINITIONS
+
+const int SD_PIN = 10; //change this to change the SD card pin number
+String signal;
+
+File data_file; 
+bool reading_on;
+bool stop;
+bool new_file_created;
+bool zero;
+bool taring;
+
+////////////////////////////////////////////////////////////////////////////////////////
+//LOAD CELL READING DEFINITIONS
+
+const int RPM_PIN = 2;
+const float MARKERS = 1;
+float RPM;
+float objects;
+long prev_second;
+bool see_object;
+
+////////////////////////////////////////////////////////////////////////////////////////
+//HELPER FUNCTIONS
+
+//called when a signal is sent from master
+void receiveEvent(int bytes){ 
+  signal = "";
+  while (Wire.available()) {  // Loop through all available data
+    char c = Wire.read();  // Read a byte
+    signal += c;  // Append to the string
+  }
+
+  if(signal == "b"){ // START data collection
+    reading_on = true;
+  }
+  else if(signal == "e"){ // STOP data collection
+    stop = true;
+    reading_on = false;
+  }
+  else if(signal == "t"){
+    taring = true;
+  }
+  else if(signal == "z"){
+    zero = true;
+  }
+  else{ //write to file
+    new_file_created = true;
+  }
+}
+
+void count(){
+  see_object = true;
+}
+
+void increment(){
+  if(see_object){
+    objects++;
+    see_object = false;
+  }
+}
+
+// Initializes Load Cell
+void init_LoadCell () {
+  Serial.println("Initializing the HX711 . . .");
+
+  TorqueSensor.begin();
+  boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
+  TorqueSensor.start(2000, _tare); //tare for 2 seconds
+  if (TorqueSensor.getTareTimeoutFlag() || TorqueSensor.getSignalTimeoutFlag()) {
+    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+    while (1);
+  }
+
+  Serial.println("Done initializing HX711");
+}
+
+void calibrate(){
+  Serial.println("Calibrating torque sensor");
+  if(TorqueSensor.update()){
+    float raw = TorqueSensor.getData();
+    TorqueSensor.setCalFactor(raw / KNOWN_TORQUE);
+  }
+  Serial.println("Done calibrating torque sensor");
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+//MAIN DRIVER CODE
+
+void setup(){
+  signal = "";
+  reading_on = false;
+  stop = false;
+  new_file_created = false;
+  zero = false;
+  taring = false;
+  RPM = 0;
+
+  pinMode(CURRENT_PIN, INPUT);
+  pinMode(VOLTAGE_PIN, INPUT);
+
+  pinMode(RPM_PIN, INPUT);
+  see_object = false;
+  attachInterrupt(digitalPinToInterrupt(RPM_PIN), count, FALLING);
+
+  //Initialize I2C protocol (slave)
+  Wire.begin(9); //Slave arduino set to address 9
+  Wire.onReceive(receiveEvent);
+
+  //Initialize Serial
+  Serial.begin(57600);
+  Serial.println("Setting up");
+
+  init_LoadCell(); //initialze the load cell
+
+  //Initialize SD card; If no file is attached or something else goes wrong, 
+  //the code put itself in an infinite loop
+  if (!SD.begin(SD_PIN)) {
+    Serial.println("Failed to initialize SD card");
+    while(1); //infinite loop to prevent further looping by loop()
+  }
+}
+
+void loop(){
+  if(taring){
+    if(zero){ 
+      calibrate();
+      zero = false;
+      taring = false;
+    }
+  }
+  else{
+    if(new_file_created){ //create a new file
+      String file_name = "Test_" + signal + ".csv";
+      data_file = SD.open(file_name, FILE_WRITE); //create the file
+      data_file.println("Current, Raw Current, Voltage, Raw Voltage, Torque, RPM"); //set up csv headers
+      new_file_created = false;
+    }
+
+    if(data_file){
+      if(reading_on){ //if a file exists and data logging/testing is turned on
+        //calculate RPM; Can increase precision by adding more markers
+        if(millis() >= prev_second + 1000){
+          RPM = (objects / MARKERS) * 60.0;
+          objects = 0;
+          prev_second = millis();
+        }
+        
+        if(TorqueSensor.update()){
+          //read in current and voltage
+          int current_value_in = analogRead(CURRENT_PIN);
+          int voltage_value_in = analogRead(VOLTAGE_PIN);
+
+          // Calculate the voltage, this is for a 5V Arduino, is that what we have?
+          float voltage = (21 * voltage_value_in);
+
+          // Calculate current
+          float current_voltage = current_value_in * (5.0 / 1024.0);
+          float current = (current_voltage - ZERO_CURRENT_VOLTAGE) / CURRENT_SENSITIVITY;
+          
+          if(millis() > last_serial_timestamp + SERIAL_PRINT_INTERVAL){     
+            float torque_data = TorqueSensor.getData();    
+            last_serial_timestamp = millis();
+
+            Serial.println("Current: " + String(current, 4) + " | Voltage: " + String(voltage, 4) + " | Torque: " +  String(torque_data) + " | RPM: " + String(RPM));
+            
+            data_file.println(String(current) + ", " + String(current_value_in) + ", " + String(voltage) + ", " + String(voltage_value_in) + ", " + String(torque_data) + ", " + String(RPM)); 
+            data_file.flush();
+          }
+        }
+        //increment a rotation counter if the tachometer reads LOW (it sees something pass)
+        increment();
+      }
+      else if(stop){ //If the signal to stop testing is recieved from master, close the file
+        data_file.close();
+        setup();
+      }
+    }
+  }
+}
